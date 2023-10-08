d1151f1dc5a4f7f22abc72047014c12eda10bcc7
Abdulali Aliyev <Abdulalialiyev@outlook.com>
2023-10-09 00:13:01 +0400

    Equal Added

--- ArraysAndLoops/src/ArrayHomeWorks.java
+++ ArraysAndLoops/src/ArrayHomeWorks.java
@@ -38,4 +38,17 @@ public class ArrayHomeWorks {
         return Numbers;
     }
 
+    static boolean IsEqual(int[] list1, int[] list2) {
+
+        if (list1.length != list2.length) {
+            return false;
+        }
+
+        for (int i = 0; i < list1.length; i++) {
+            if (list1[i] != list2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- ArraysAndLoops/src/Main.java
+++ ArraysAndLoops/src/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         int[] Numbers = {-1,-34,0,4,49,-55,-9,49,-7,4,7,8,55};
+        int[] List1 = {1,2,3,4};
+        int[] List2 = {1,2,3,4};
+        int[] List3 = {1,2,3,3};
 
         // Get the largest value and repeats
         ArrayHomeWorks.GetLargest(Numbers);
@@ -16,6 +19,10 @@ public class Main {
             System.out.println(ArrayHomeWorks.SortArray(Numbers)[i]);
 
         }
+
+        // Check Equality
+        System.out.println(ArrayHomeWorks.IsEqual(List1,List2));
+        System.out.println(ArrayHomeWorks.IsEqual(List1,List3));
         }
 
     }
