d2332b04fcb6fdadb77b64888df478025fe87abb
Abdulali Aliyev <Abdulalialiyev@outlook.com>
2023-10-12 16:40:34 +0400

    Employee & HrManager
    Employee Class added

---
+++ ObjectOrientedFirst/src/Employee.java
@@ -0,0 +1,29 @@
+public class Employee {
+
+    String name;
+    double salary;
+
+    public Employee(String name, double salary){
+        this.name = name;
+        this.salary = salary;
+
+    }
+
+    public Employee(String name){
+        this.name = name;
+
+    }
+
+    public Employee(){
+
+    }
+
+    public static void Work(){
+
+
+    }
+
+    public double GetSalary(){
+        return salary;
+    }
+}
---
+++ ObjectOrientedFirst/src/StaticVariables.java
@@ -0,0 +1,6 @@
+public class StaticVariables {
+
+    public int a;
+    public static int b;
+
+}
