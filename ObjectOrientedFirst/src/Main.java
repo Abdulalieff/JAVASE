d724455603c7e0d002e5f2a240aed763f3e9cd35
Abdulali Aliyev <Abdulalialiyev@outlook.com>
2023-10-12 17:00:48 +0400

    static word added
    static word added

--- ObjectOrientedFirst/src/Main.java
+++ ObjectOrientedFirst/src/Main.java
@@ -3,14 +3,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        String staticwords  = "'s salary is: ";
+        
         Employee employee = new Employee("Ali", 100);
         HRManager hrmanager = new HRManager("Zahra", 200);
 
         employee.Work();
-        System.out.println(employee.name + "'s salary is: " + employee.GetSalary());
+        System.out.println(employee.name + staticwords + employee.GetSalary());
 
         hrmanager.Work();
-        System.out.println(hrmanager.name + "'s salary is: " + hrmanager.GetSalary());
+        System.out.println(hrmanager.name + staticwords + hrmanager.GetSalary());
 
         hrmanager.AddEmployee(new Employee("John",500));
 
